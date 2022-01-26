85a431b70cad4c7e8f814600613f34803e987f3f
EduMogro <edumogro@outlook.com>
2022-01-26 15:45:42 -0300

    feat: sort posts by most recent

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        // $posts = Post::all();
+        $posts = Post::latest()->get();
 
         return $posts;
     }
