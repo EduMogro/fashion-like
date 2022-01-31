a7e010e6911e7b2701b9631ff1b7e85acd9ee31a
EduMogro <edumogro@outlook.com>
2022-01-31 13:55:37 -0300

    feat: add authentication security

--- routes/api.php
+++ routes/api.php
@@ -14,22 +14,28 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function ($router) {
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
-    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
-    Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
-    Route::post('me', 'App\Http\Controllers\AuthController@me');
     Route::post('register', 'App\Http\Controllers\AuthController@register');
+    Route::post('login', 'App\Http\Controllers\AuthController@login');
+    Route::post('me', 'App\Http\Controllers\AuthController@me');
+    Route::post('logout', 'App\Http\Controllers\AuthController@logout');
+    // Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
 });
 
+
+// POSTS
+
 Route::get('/posts', 'App\Http\Controllers\PostController@index');
-Route::post('/posts', 'App\Http\Controllers\PostController@store');
-Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update');
-Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy');
+
+// Route::group(['middleware' => ['jwt.verify']], function() {
+    Route::post('/posts', 'App\Http\Controllers\PostController@store');
+    Route::put('/posts/{id}', 'App\Http\Controllers\PostController@update');
+    Route::delete('/posts/{id}', 'App\Http\Controllers\PostController@destroy');
+// });
